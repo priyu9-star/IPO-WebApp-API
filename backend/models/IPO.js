2282ca4325f7663c600835500ff014c7dda6e14a
Ayesha0731 <ayeshasiddikha817@gmail.com>
2025-04-04 21:53:24 +0530

    Update IPO.js

--- backend/models/IPO.js
+++ backend/models/IPO.js
@@ -29,3 +29,41 @@ const IPO = sequelize.define("IPO", {
 });
 
 module.exports = IPO;
+
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../config/db");
+
+const Broker = sequelize.define("Broker", {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  company: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
+  },
+  phone: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  licenseNumber: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  status: {
+    type: DataTypes.ENUM("active", "inactive"),
+    allowNull: false,
+    defaultValue: "active",
+  }
+});
+
+module.exports = Broker;
